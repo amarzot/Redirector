79111fa72c024419cee4498e43322a9429fc3a60
Alexander Marzot <43010352+amarzot@users.noreply.github.com>
2024-02-03 00:21:18 -0500

    Add Create and Delete

--- options.js
+++ options.js
@@ -1,21 +1,39 @@
-const DEFAULT_REDIRECTS = [
-    { from: { hostname: "twitter.com" }, to: { hostname: "nitter.freedit.eu" } },
-    { from: { hostname: "gist.github.com" }, to: { hostname: "gothub.dev.projectsegfau.lt", pathname: "gist{pathname}" } },
-    { from: { hostname: "github.com" }, to: { hostname: "gothub.dev.projectsegfau.lt" } },
-];
+function removeEmptyEntries(obj) {
+    return Object.fromEntries(Object.entries(obj).filter(e => e[1]));
+}
+function getRedirect() {
+    const form = document.querySelector("form");
+    const getFrom = (id_part) => form.querySelector(`input#from-${id_part}`).value;
+    const getTo = (id_part) => form.querySelector(`input#to-${id_part}`).value;
+
+    const from = {
+        hostname: getFrom("hostname"),
+        pathname: getFrom("pathname")
+    };
+    const to = {
+        hostname: getTo("hostname"),
+        pathname: getTo("pathname")
+    };
+    return {
+        from: removeEmptyEntries(from),
+        to: removeEmptyEntries(to)
+    };
+}
 
 
 async function saveRedirect(e) {
     e.preventDefault();
-    await browser.storage.sync.set({
-        redirects: DEFAULT_REDIRECTS
-    });
+    const res = await browser.storage.sync.get('redirects');
+    const redirects = res.redirects || [];
+    redirects.unshift(getRedirect());
+    document.querySelector("form").reset();
+    await browser.storage.sync.set({ redirects });
     restoreOptions();
 }
 
 async function restoreOptions() {
     const res = await browser.storage.sync.get('redirects');
-    if (!("redirects" in res)) return;
+    if (!res.hasOwnProperty("redirects")) return;
 
     const select = document.querySelector("select");
     select.replaceChildren();
@@ -27,6 +45,7 @@ async function restoreOptions() {
     select.append(...options);
 }
 
+//Unused currently
 function editRedirection({ from, to }) {
     const form = document.querySelector("form");
     form.reset();
@@ -38,5 +57,21 @@ function editRedirection({ from, to }) {
     });
 }
 
+async function deleteSelected() {
+    const select = document.querySelector("select");
+    const indicesToDelete = Object.values(select.selectedOptions).map(o => o.index);
+    const maxIndex = indicesToDelete.reduce((a, n) => n > a ? n : a, -1);
+    const res = await browser.storage.sync.get('redirects');
+    const redirects = res.redirects || [];
+    if (maxIndex >= redirects.length) {
+        console.error("Bad indices");
+    }
+    await browser.storage.sync.set({
+        redirects: redirects.filter((_, i) => !indicesToDelete.includes(i))
+    });
+    restoreOptions();
+}
+
 document.addEventListener('DOMContentLoaded', restoreOptions);
 document.querySelector("form").addEventListener("submit", saveRedirect);
+document.querySelector("button#delete").addEventListener("click", deleteSelected);
