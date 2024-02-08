8d4d6bc67d7f8780034efd5349841d5ac2b18e58
Alexander Marzot <43010352+amarzot@users.noreply.github.com>
2024-02-08 13:49:50 -0500

    Add action, Add button functionality

--- options.js
+++ options.js
@@ -1,6 +1,7 @@
 function removeEmptyEntries(obj) {
     return Object.fromEntries(Object.entries(obj).filter(e => e[1]));
 }
+
 function getRedirect() {
     const form = document.querySelector("form");
     const getFrom = (id_part) => form.querySelector(`input#from-${id_part}`).value;
@@ -20,17 +21,6 @@ function getRedirect() {
     };
 }
 
-
-async function saveRedirect(e) {
-    e.preventDefault();
-    const res = await browser.storage.sync.get('redirects');
-    const redirects = res.redirects || [];
-    redirects.unshift(getRedirect());
-    document.querySelector("form").reset();
-    await browser.storage.sync.set({ redirects });
-    restoreOptions();
-}
-
 async function restoreOptions() {
     const res = await browser.storage.sync.get('redirects');
     if (!res.hasOwnProperty("redirects")) return;
@@ -45,8 +35,69 @@ async function restoreOptions() {
     select.append(...options);
 }
 
-//Unused currently
-function editRedirection({ from, to }) {
+async function saveRedirect() {
+    const res = await browser.storage.sync.get('redirects');
+    const redirects = res.redirects || [];
+    redirects.unshift(getRedirect());
+    document.querySelector("form").reset();
+    await browser.storage.sync.set({ redirects });
+    restoreOptions();
+}
+
+async function overwriteSelected() {
+    const selectedIndex = document.querySelector("select").selectedIndex;
+    const res = await browser.storage.sync.get('redirects');
+    if (!res.hasOwnProperty("redirects")) {
+        console.error("No redirects found", res);
+        return;
+    }
+    const redirects = res.redirects;
+    if (selectedIndex >= redirects.length) {
+        console.error("Bad index", selectedIndex);
+        return;
+    }
+    redirects[selectedIndex] = getRedirect();
+    document.querySelector("form").reset();
+    await browser.storage.sync.set({ redirects });
+    restoreOptions();
+}
+
+
+async function deleteSelected() {
+    const select = document.querySelector("select");
+    const indicesToDelete = Object.values(select.selectedOptions).map(o => o.index);
+    const maxIndex = indicesToDelete.reduce((a, n) => n > a ? n : a, -1);
+
+    const res = await browser.storage.sync.get('redirects');
+    if (!res.hasOwnProperty("redirects")) return;
+
+    const redirects = res.redirects;
+    if (maxIndex >= redirects.length) {
+        console.error("Bad indices");
+        return;
+    }
+    await browser.storage.sync.set({
+        redirects: redirects.filter((_, i) => !indicesToDelete.includes(i))
+    });
+    restoreOptions();
+}
+
+async function loadSelected() {
+    const select = document.querySelector("select");
+    const loadIndex = select.selectedIndex;
+
+    const res = await browser.storage.sync.get('redirects');
+    if (!res.hasOwnProperty("redirects")) {
+        console.error("No redirects found", res);
+        return;
+    }
+
+    const redirects = res.redirects;
+    if (loadIndex >= redirects.length) {
+        console.error("Bad index", loadIndex);
+        return;
+    }
+    const { from, to } = redirects[loadIndex];
     const form = document.querySelector("form");
     form.reset();
     Object.keys(from).forEach(function(name) {
@@ -57,21 +108,18 @@ function editRedirection({ from, to }) {
     });
 }
 
-async function deleteSelected() {
-    const select = document.querySelector("select");
-    const indicesToDelete = Object.values(select.selectedOptions).map(o => o.index);
-    const maxIndex = indicesToDelete.reduce((a, n) => n > a ? n : a, -1);
-    const res = await browser.storage.sync.get('redirects');
-    const redirects = res.redirects || [];
-    if (maxIndex >= redirects.length) {
-        console.error("Bad indices");
-    }
-    await browser.storage.sync.set({
-        redirects: redirects.filter((_, i) => !indicesToDelete.includes(i))
-    });
+document.addEventListener('DOMContentLoaded', function() {
     restoreOptions();
-}
+    const saveButton = document.querySelector("button#save");
+    const overwriteButton = document.querySelector("button#overwrite");
+    const loadButton = document.querySelector("button#load")
+    const deleteButton = document.querySelector("button#delete")
 
-document.addEventListener('DOMContentLoaded', restoreOptions);
-document.querySelector("form").addEventListener("submit", saveRedirect);
-document.querySelector("button#delete").addEventListener("click", deleteSelected);
+    const select = document.querySelector("select");
+    console.log("select", select.selectedIndex);
+
+    saveButton.addEventListener("click", saveRedirect);
+    overwriteButton.addEventListener("click", overwriteSelected);
+    loadButton.addEventListener("click", loadSelected);
+    deleteButton.addEventListener("click", deleteSelected);
+});
